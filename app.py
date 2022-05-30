a19958b2374f954dd197c494ac75bda5f32c4806
Cintakins <jacintalerwill@hotmail.com>
2022-05-30 14:01:36 +0000

    added base.html and extended it with the recipes.html file

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo 
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -13,9 +15,14 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "hello World..."
+@app.route("/get_recipes")
+def get_recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
