77dacf73f4ed49148e8c98b4de503891092d330a
Cintakins <jacintalerwill@hotmail.com>
2022-05-31 14:29:59 +0000

    added some html content from materialize to register.html

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo 
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -36,6 +37,10 @@ def get_home():
 def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
