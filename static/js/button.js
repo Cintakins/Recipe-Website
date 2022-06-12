5d3c4bf8623bb3e728aef707df4ef02f220cff9b
Cintakins <jacintalerwill@hotmail.com>
2022-06-12 21:27:16 +0000

    changed some code for add step button

--- static/js/button.js
+++ static/js/button.js
@@ -1,17 +1,8 @@
-var instructionsButton = document.getElementsByClass("add-step");
+document.getElementById("add-step").addEventListener('click', addStep());
 
-
-for (let i = 0; i < instructionsButton.length; i++) {
-    instructionsButton[i].addEventListener('click', addStep());
-}
-
-function addstep() {
-    document.getElementById("step-div").innerHTML = `
-  <div class='input-field col s12'>
-  <textarea id='textarea1' class='materialize-textarea' name='instructions1'></textarea>
-  <label for='instructions1'>Instructions: Step 1</label>
-  <a class='waves-effect waves-light btn red darken-1 btn-small add-step'>
-      <i class='material-icons right'>add_circle_outline</i>Add Step
-  </a>
-</div>`
+function addStep() {
+    instructionStep = `
+    <textarea id='textarea1' class='materialize-textarea' name='instructions1'></textarea>
+    <label for='instructions1'>Instructions: Step 1</label> `
+    document.getElementById("step-div").appendChild(instructionStep);
 };
