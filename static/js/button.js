e7f33d41e088bc4d50a90fc95c67733c60bff1d8
Cintakins <jacintalerwill@hotmail.com>
2022-06-13 13:41:23 +0000

    tried to add javascript to creat extra form elements but it doesn't work yet

--- static/js/button.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("add-step").addEventListener('click', addStep());
-
-function addStep() {
-    instructionStep = `
-    <textarea id='textarea1' class='materialize-textarea' name='instructions1'></textarea>
-    <label for='instructions1'>Instructions: Step 1</label> `
-    document.getElementById("step-div").appendChild(instructionStep);
-};
--- static/js/script.js
+++ static/js/script.js
@@ -11,4 +11,21 @@ $(document).ready(function () {
   $('select').formSelect();
 })
 
+var counter = 0
 
+// code from https://www.quirksmode.org/dom/domform.html
+function addStep() {
+  counter++;
+  var newSteps = document.getElementById('step-div').cloneNode(true);
+  newSteps.id = ''
+  newSteps.style.display = 'block';
+  var newStep = newSteps.childNodes;
+  for (var i=0;i<newStep.length;i++) {
+		var step = newField[i].name
+		if (step)
+			newField[i].name = step + counter;
+	}
+
+  var insertHere = document.getElementById('create-step');
+	insertHere.parentNode.insertBefore(newSteps,insertHere);
+};
